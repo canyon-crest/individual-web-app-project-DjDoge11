d2e706dc0127b66ba5c14ce7b54c8f8e0ac58493
Parker <146790960+DjDoge11@users.noreply.github.com>
2026-03-04 20:01:38 +0000

    fixed the buttons

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // add javascript here
-document.getElementById("contactBtn").addEventListener("click", () => {
+document.getElementById("contactBtn")?.addEventListener("click", () => {
     const contact = document.getElementById("contactText");
     contact.toggleAttribute("hidden");
   });
-document.getElementById("myButton").addEventListener("click", () => {
+document.getElementById("myButton")?.addEventListener("click", () => {
     const nothing = document.getElementById("myBtnText");
     nothing.toggleAttribute("hidden");
   });
